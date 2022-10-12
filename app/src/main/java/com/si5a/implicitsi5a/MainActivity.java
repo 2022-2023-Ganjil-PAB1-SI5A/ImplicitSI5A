c8ef4c52b38e43912e414565be8aaab1e0cfe373
ahmfarisi <ahmfarisi@gmail.com>
2022-10-12 17:00:38 +0700

    Finish First Button Listener

--- app/src/main/java/com/si5a/implicitsi5a/MainActivity.java
+++ app/src/main/java/com/si5a/implicitsi5a/MainActivity.java
@@ -2,13 +2,56 @@ package com.si5a.implicitsi5a;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText etUrl, etLokasi, etTeks;
+    Button btnBukaWebsite, btnBukaLokasi, btnBagikanTeks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etUrl = findViewById(R.id.et_url);
+        etLokasi = findViewById(R.id.et_lokasi);
+        etTeks = findViewById(R.id.et_teks);
+        btnBukaWebsite = findViewById(R.id.btn_buka_website);
+        btnBukaLokasi = findViewById(R.id.btn_buka_lokasi);
+        btnBagikanTeks = findViewById(R.id.btn_bagikan_teks);
+
+        btnBukaWebsite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String urlWebsite = etUrl.getText().toString();
+                Uri uriUrlWebsite = Uri.parse(urlWebsite);
+                Intent intentWebsite = new Intent(Intent.ACTION_VIEW, uriUrlWebsite);
+                try {
+                    startActivity(intentWebsite);
+                }catch (Exception e){
+                    etUrl.setError("Url Tidak Sesuai!");
+                }
+
+            }
+        });
+
+        btnBukaLokasi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnBagikanTeks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
